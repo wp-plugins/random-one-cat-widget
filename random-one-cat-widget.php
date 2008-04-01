4f853adc2fa8c8d6fd7d914ffc10d82e69cff4e9
BrokenCrust <BrokenCrust@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-01 19:31:24 +0000

    git-svn-id: https://plugins.svn.wordpress.org/random-one-cat-widget/trunk@37567 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- random-one-cat-widget.php
+++ random-one-cat-widget.php
@@ -3,7 +3,7 @@
 Plugin Name: Random One Cat Widget
 Description: This Widget shows a single random post from a given category.
 Author: BrokenCrust
-Version: 0.2
+Version: 0.3
 Author URI: http://www.brokencrust.eu/
 Plugin URI: http://www.brokencrust.eu/index.php?name=Forums&file=viewforum&f=21
 */
@@ -12,16 +12,23 @@ function widget_random_one_cat_init() {
 
 	if (!function_exists('register_sidebar_widget')) { return; }
 
-	function widget_random_one_cat($args, $number = 1) {
-		if ($output = wp_cache_get('widget_random_one_cat')) { return print($output); }
-
-		extract($args);
+	function widget_random_one_cat($args, $widget_args = 1) {
+		extract($args, EXTR_SKIP);
+		if (is_numeric($widget_args)) {
+			$widget_args = array( 'number' => $widegt_args );
+		}
+		$widget_args = wp_parse_args($widget_args, array('number' => -1));
+		extract($widget_args, EXTR_SKIP);
 
 		$options = get_option('widget_random_one_cat');
 
-		$title = $options[$number]['title'] ? $options[$number]['title'] : __('Random Post');
-		$show_post_title = $options[$number]['show_post_title'] ? 1 : 0;
-		$category = $options[$number]['category'] ? intval($options[$number]['category']) : 1;
+		if (!isset($options[$number])) { return; }
+
+		if (isset($options[$number]['error']) && $options[$number]['error']) { return; }
+
+		$title = $options[$number]['title'];
+		$category = intval($options[$number]['category']);
+		$show_post_title = $options[$number]['show_post_title'];
 
 		$rand = get_posts('numberposts=1&category='.$category.'&orderby=RAND()');
 
@@ -34,108 +41,151 @@ function widget_random_one_cat_init() {
 		}
 		echo $rand[0]->post_content;
 		echo $after_widget;
-        }
+	}
 
-	function widget_random_one_cat_control($number) {
+	function widget_random_one_cat_control($widget_args) {
+		global $wp_registered_widgets;
+		static $updated = false;
 
-		$options = $newoptions = get_option('widget_random_one_cat');
-
-		if ( $_POST['random_one_cat-submit-'.$number] ) {
-			$newoptions[$number]['title'] = strip_tags(stripslashes($_POST["random_one_cat-title-$number"]));
-			$newoptions[$number]['category'] = intval($_POST["random_one_cat-category-$number"]);
-			$newoptions[$number]['show_post_title'] = ($_POST["random_one_cat-show_post_title-$number"]);
+		if (is_numeric($widget_args)) {
+			$widget_args = array( 'number' => $widget_args );
 		}
+		$widget_args = wp_parse_args($widget_args, array('number' => -1));
+		extract($widget_args, EXTR_SKIP);
 
-		if ( $options != $newoptions ) {
-			$options = $newoptions;
-			update_option('widget_random_one_cat', $options);
+		$options = get_option('widget_random_one_cat');
+		if (!is_array($options)) {
+			$options = array();
 		}
+		if (!$updated && 'POST' == $_SERVER['REQUEST_METHOD'] && !empty($_POST['sidebar'])) {
+			$sidebar = (string) $_POST['sidebar'];
 
-		$title = attribute_escape($options[$number]['title']);
-		$category = $options[$number]['category'] ? $options[$number]['category'] : 1;
-		$show_post_title = $options[$number]['show_post_title'] ? 'checked="checked"' : '';
+			$sidebars_widgets = wp_get_sidebars_widgets();
+			if (isset($sidebars_widgets[$sidebar])) {
+				$this_sidebar =& $sidebars_widgets[$sidebar];
+			} else {
+				$this_sidebar = array();
+			}
 
-?>
-	<p>
-		<label for="random_one_cat-title-<?php echo "$number"; ?>">
-			<?php _e('Title:'); ?>
-			<input style="width: 250px;" id="random_one_cat-title-<?php echo "$number"; ?>" name="random_one_cat-title-<?php echo "$number"; ?>" type="text" value="<?php echo $title; ?>" />
-		</label>
-	</p>
-	<p>
-		<label for="random_one_cat-category-<?php echo "$number"; ?>">
-			<?php _e('Category:'); ?>
-			<select name="random_one_cat-category-<?php echo "$number"; ?>" id="random_one_cat-category-<?php echo "$number"; ?>">
-				<?php
-					$categories = get_categories('');
-					foreach ($categories as $cat) {
-						if ($cat->cat_ID == $category) {
-							$option = '<option selected="selected" value="'.$cat->cat_ID.'">';
-						} else {
-							$option = '<option value="'.$cat->cat_ID.'">';
-						}
-						$option .= $cat->cat_name.'</option>';
-						echo $option;
+			foreach ($this_sidebar as $_widget_id) {
+				if ('widget_random_one_cat' == $wp_registered_widgets[$_widget_id]['callback'] && isset($wp_registered_widgets[$_widget_id]['params'][0]['number']) ) {
+					$widget_number = $wp_registered_widgets[$_widget_id]['params'][0]['number'];
+					if (!in_array("random_one_cat-$widget_number", $_POST['widget-id'])) {
+						unset($options[$widget_number]);
 					}
-				?>
-			</select>
-		</label>
-	</p>
-	<p>
-		<label for="random_one_cat-show_post_title-<?php echo "$number"; ?>">
-			<?php _e('Show Post Title'); ?>
-			<input class="checkbox" type="checkbox" <?php echo $show_post_title; ?> id="random_one_cat-show_post_title-<?php echo "$number"; ?>" name="random_one_cat-show_post_title-<?php echo "$number"; ?>" />
-		</label>
-	</p>
-	<input type="hidden" id="random_one_cat-submit-<?php echo "$number"; ?>" name="random_one_cat-submit-<?php echo "$number"; ?>" value="1" />
-<?php
+				}
+			}
+
+			foreach((array) $_POST['widget-random_one_cat'] as $widget_number => $widget_random_one_cat) {
+				$widget_random_one_cat = stripslashes_deep($widget_random_one_cat);
+				$options[$widget_number] = widget_random_one_cat_process($widget_random_one_cat);
+			}
+
+			update_option('widget_random_one_cat', $options);
+			$updated = true;
+		}
+
+		if (-1 == $number) {
+			$title           = __('Random Post');
+			$category        = 1;
+			$show_post_title = 1;
+			$error           = false;
+			$number          = '%i%';
+		} else {
+			extract((array) $options[$number]);
+		}
+
+		widget_random_one_cat_form(compact('number', 'title', 'category', 'show_post_title'));
 	}
 	widget_random_one_cat_register();
 }
 
-function widget_random_one_cat_setup() {
-	$options = $newoptions = get_option('widget_random_one_cat');
-	if ( isset($_POST['random_one_cat-number-submit']) ) {
-		$number = (int) $_POST['random_one_cat-number'];
-		if ( $number > 9 ) $number = 9;
-		if ( $number < 1 ) $number = 1;
-		$newoptions['number'] = $number;
-	}
-	if ( $options != $newoptions ) {
-		$options = $newoptions;
-		update_option('widget_random_one_cat', $options);
-		widget_random_one_cat_register($options['number']);
-	}
+function widget_random_one_cat_process($widget_random_one_cat) {
+	$title           = trim(strip_tags($widget_random_one_cat['title']));
+	$category        = (int) $widget_random_one_cat['category'];
+	$show_post_title = (int) $widget_random_one_cat['show_post_title'];
+
+	return compact('title', 'category', 'show_post_title');
 }
 
-function widget_random_one_cat_page() {
-	$options = $newoptions = get_option('widget_random_one_cat');
-?>
-	<div class="wrap">
-		<form method="POST">
-		<h2>Random One Cat Widgets</h2>
-		<p style="line-height: 30px;"><?php _e('How many Random One Cat widgets would you like?'); ?>
-		<select id="random_one_cat-number" name="random_one_cat-number" value="<?php echo $options['number']; ?>">
-<?php for ( $i = 1; $i < 10; ++$i ) echo "<option value='$i' ".($options['number']==$i ? "selected='selected'" : '').">$i</option>"; ?>
-		</select>
-		<span class="submit"><input type="submit" name="random_one_cat-number-submit" id="random_one_cat-number-submit" value="<?php _e('Save'); ?>" /></span></p>
-		</form>
-	</div>
-<?php
+function widget_random_one_cat_form($args, $inputs = null) {
+	$default_inputs = array('title' => true, 'category' => true, 'show_post_title' => true);
+	$inputs = wp_parse_args($inputs, $default_inputs);
+	extract( $args );
+	$number          = attribute_escape($number);
+	$title           = attribute_escape($title);
+	$category        = attribute_escape($category);
+	$show_post_title = (int) $show_post_title;
+
+	if ($inputs['title']) {
+		?>
+			<p>
+				<label for="random_one_cat-title-<?php echo $number; ?>"><?php _e('Enter the widget title:'); ?>
+				<input class="widefat" id="random_one_cat-title-<?php echo $number; ?>" name="widget-random_one_cat[<?php echo $number; ?>][title]" type="text" value="<?php echo $title; ?>" />
+			</label>
+			</p>
+		<?php
+	}
+	if ($inputs['category']) {
+		?>
+			<p>
+				<label for="random_one_cat-category-<?php echo $number; ?>"><?php _e('Select a category:'); ?><br />
+					<select id="random_one_cat-category-<?php echo $number; ?>" name="widget-random_one_cat[<?php echo $number; ?>][category]">
+						<?php
+							$categories = get_categories('');
+							foreach ($categories as $cat) {
+								if ($cat->cat_ID == $category) {
+									$option = '<option selected="selected" value="'.$cat->cat_ID.'">';
+								} else {
+									$option = '<option value="'.$cat->cat_ID.'">';
+								}
+								$option .= $cat->cat_name.'</option>';
+								echo $option;
+							}
+						?>
+					</select>
+				</label>
+			</p>
+		<?php
+	}
+	if ($inputs['show_post_title']) {
+		?>
+			<p>
+				<label for="random_one_cat-show_post_title-<?php echo $number; ?>">
+					<input id="random_one_cat-show_post_title-<?php echo $number; ?>" name="widget-random_one_cat[<?php echo $number; ?>][show_post_title]" type="checkbox" value="1" <?php if ( $show_post_title ) echo 'checked="checked"'; ?>/>
+					<?php _e('Show Post Title?'); ?>
+				</label>
+			</p>
+		<?php
+	}
+	foreach (array_keys($default_inputs) as $input) {
+		if ( 'hidden' === $inputs[$input] ) {
+			$id = str_replace('_', '-', $input);
+			?>
+				<input type="hidden" id="random_one_cat-<?php echo $id; ?>-<?php echo $number; ?>" name="widget-random_one_cat[<?php echo $number; ?>][<?php echo $input; ?>]" value="<?php echo $$input; ?>" />
+			<?php
+		}
+	}
 }
 
 function widget_random_one_cat_register() {
-	$options = get_option('widget_random_one_cat');
-	$number = $options['number'];
-	if ( $number < 1 ) $number = 1;
-	if ( $number > 9 ) $number = 9;
-	for ($i = 1; $i <= 9; $i++) {
-		$name = array('Random One Cat %s', null, $i);
-		register_sidebar_widget($name, $i <= $number ? 'widget_random_one_cat' : /* unregister */ '', $i);
-		register_widget_control($name, $i <= $number ? 'widget_random_one_cat_control' : /* unregister */ '', 90, 300, $i);
+	if (!$options = get_option('widget_random_one_cat')) {
+		$options = array();
 	}
-	add_action('sidebar_admin_setup', 'widget_random_one_cat_setup');
-	add_action('sidebar_admin_page', 'widget_random_one_cat_page');
+	$widget_ops = array('classname' => 'widget_random_one_cat', 'description' => __( 'Display a random post from one category.' ));
+	$control_ops = array('id_base' => 'random_one_cat');
+	$name = __('Random One Cat');
+	$id = false;
+	foreach (array_keys($options) as $o) {
+		if (!isset($options[$o]['title']) || !isset($options[$o]['category']) || !isset($options[$o]['show_post_title'])) { continue; }
+		$id = "random_one_cat-$o";
+		wp_register_sidebar_widget($id, $name, 'widget_random_one_cat', $widget_ops, array( 'number' => $o ));
+		wp_register_widget_control($id, $name, 'widget_random_one_cat_control', $control_ops, array( 'number' => $o ));
+	}
+	if (!$id) {
+		wp_register_sidebar_widget('random_one_cat-1', $name, 'widget_random_one_cat', $widget_ops, array( 'number' => -1 ));
+		wp_register_widget_control('random_one_cat-1', $name, 'widget_random_one_cat_control', $control_ops, array( 'number' => -1 ));
+	}
 }
 
 add_action('widgets_init', 'widget_random_one_cat_init');
