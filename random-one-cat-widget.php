dacb735c865ac963e039760bb550946fb32d5c66
BrokenCrust <BrokenCrust@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-06-18 17:02:37 +0000

    git-svn-id: https://plugins.svn.wordpress.org/random-one-cat-widget/trunk@51078 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- random-one-cat-widget.php
+++ random-one-cat-widget.php
@@ -3,7 +3,7 @@
 Plugin Name: Random One Cat Widget
 Description: This Widget shows a single random post from a given category.
 Author: BrokenCrust
-Version: 0.3
+Version: 0.4
 Author URI: http://www.brokencrust.eu/
 Plugin URI: http://www.brokencrust.eu/index.php?name=Forums&file=viewforum&f=21
 */
@@ -29,6 +29,7 @@ function widget_random_one_cat_init() {
 		$title = $options[$number]['title'];
 		$category = intval($options[$number]['category']);
 		$show_post_title = $options[$number]['show_post_title'];
+                $show_custom_fields = $options[$number]['show_custom_fields'];
 
 		$rand = get_posts('numberposts=1&category='.$category.'&orderby=RAND()');
 
@@ -40,6 +41,23 @@ function widget_random_one_cat_init() {
 			<?php
 		}
 		echo $rand[0]->post_content;
+
+		if ($show_custom_fields == 1) {
+			$custom_fields = get_post_custom($rand[0]->ID);
+			if (count($custom_fields) > 0) {
+				echo '<ul class="post-meta">';
+				foreach ($custom_fields as $key => $values) {
+					if (substr($key, 0, 1) != '_') {
+						$valueline = '';
+						foreach ($values as $i => $value) {
+							$valueline .= $value.', ';
+						}
+						echo '<li><span class="post-meta-key">'.$key.': </span>'.trim($valueline, ', ').'</li>';
+					}
+				}
+				echo '</ul>';
+			}
+		}
 		echo $after_widget;
 	}
 
@@ -86,36 +104,39 @@ function widget_random_one_cat_init() {
 		}
 
 		if (-1 == $number) {
-			$title           = __('Random Post');
-			$category        = 1;
-			$show_post_title = 1;
-			$error           = false;
-			$number          = '%i%';
+			$title              = __('Random Post');
+			$category           = 1;
+			$show_post_title    = 1;
+			$show_custom_fields = 1;
+			$error              = false;
+			$number             = '%i%';
 		} else {
 			extract((array) $options[$number]);
 		}
 
-		widget_random_one_cat_form(compact('number', 'title', 'category', 'show_post_title'));
+		widget_random_one_cat_form(compact('number', 'title', 'category', 'show_post_title', 'show_custom_fields'));
 	}
 	widget_random_one_cat_register();
 }
 
 function widget_random_one_cat_process($widget_random_one_cat) {
-	$title           = trim(strip_tags($widget_random_one_cat['title']));
-	$category        = (int) $widget_random_one_cat['category'];
-	$show_post_title = (int) $widget_random_one_cat['show_post_title'];
+	$title              = trim(strip_tags($widget_random_one_cat['title']));
+	$category           = (int) $widget_random_one_cat['category'];
+	$show_post_title    = (int) $widget_random_one_cat['show_post_title'];
+	$show_custom_fields = (int) $widget_random_one_cat['show_custom_fields'];
 
-	return compact('title', 'category', 'show_post_title');
+	return compact('title', 'category', 'show_post_title', 'show_custom_fields');
 }
 
 function widget_random_one_cat_form($args, $inputs = null) {
-	$default_inputs = array('title' => true, 'category' => true, 'show_post_title' => true);
+	$default_inputs = array('title' => true, 'category' => true, 'show_post_title' => true, 'show_custom_fields' => true);
 	$inputs = wp_parse_args($inputs, $default_inputs);
 	extract( $args );
-	$number          = attribute_escape($number);
-	$title           = attribute_escape($title);
-	$category        = attribute_escape($category);
-	$show_post_title = (int) $show_post_title;
+	$number             = attribute_escape($number);
+	$title              = attribute_escape($title);
+	$category           = attribute_escape($category);
+	$show_post_title    = (int) $show_post_title;
+	$show_custom_fields = (int) $show_custom_fields;
 
 	if ($inputs['title']) {
 		?>
@@ -158,6 +179,16 @@ function widget_random_one_cat_form($args, $inputs = null) {
 			</p>
 		<?php
 	}
+	if ($inputs['show_custom_fields']) {
+		?>
+			<p>
+				<label for="random_one_cat-show_custom_fields-<?php echo $number; ?>">
+					<input id="random_one_cat-show_custom_fields-<?php echo $number; ?>" name="widget-random_one_cat[<?php echo $number; ?>][show_custom_fields]" type="checkbox" value="1" <?php if ( $show_custom_fields ) echo 'checked="checked"'; ?>/>
+					<?php _e('Show Custom Fields?'); ?>
+				</label>
+			</p>
+		<?php
+	}
 	foreach (array_keys($default_inputs) as $input) {
 		if ( 'hidden' === $inputs[$input] ) {
 			$id = str_replace('_', '-', $input);
@@ -177,7 +208,7 @@ function widget_random_one_cat_register() {
 	$name = __('Random One Cat');
 	$id = false;
 	foreach (array_keys($options) as $o) {
-		if (!isset($options[$o]['title']) || !isset($options[$o]['category']) || !isset($options[$o]['show_post_title'])) { continue; }
+		if (!isset($options[$o]['title']) || !isset($options[$o]['category']) || !isset($options[$o]['show_post_title']) || !isset($options[$o]['show_custom_fields'])) { continue; }
 		$id = "random_one_cat-$o";
 		wp_register_sidebar_widget($id, $name, 'widget_random_one_cat', $widget_ops, array( 'number' => $o ));
 		wp_register_widget_control($id, $name, 'widget_random_one_cat_control', $control_ops, array( 'number' => $o ));
