6a6845a096c07271fe6d0605d0129cc3be981f6f
Fecundvs <Fecundvs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-08 19:08:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/random-one-cat-widget/trunk@203312 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- random-one-cat-widget.php
+++ random-one-cat-widget.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Random One Cat Widget
 Description: This Widget shows a single random post from a given category.
-Author: Fecundvs
+Author: Fecûndvs
 Version: 1.2
 Author URI: http://fecundvs.com/
 Plugin URI: http://wordpress.org/extend/plugins/random-one-cat-widget/
